634b784c8c487d603bc7f4e6bcee81f03f232ca9
Tainá Santos <152083286+TRS2006@users.noreply.github.com>
2025-03-28 19:24:35 -0300

    Add files via upload

---
+++ script/desafio.js
@@ -0,0 +1,21 @@
+ let nomeDoHeroi = "Aquiles";
+ let XP = 300;
+
+ if (XP < 1000) {
+     console.log(`${nomeDoHeroi} é classificado como: Ferro`)
+ } else if (XP >= 1001 && XP <= 2000) {
+     console.log(`${nomeDoHeroi} é classificado como: Bronze`)
+ } else if (XP >= 2001 && XP <= 5000) {
+     console.log(`${nomeDoHeroi} é classificado como: Prata`)
+ } else if (XP >= 5001 && XP <= 7000) {
+     console.log(`${nomeDoHeroi} é classificado como: Ouro`)
+ } else if (XP >= 7001 && XP <= 8000) {
+     console.log(`${nomeDoHeroi} é classificado como: Platina`)
+ } else if (XP >= 9001 && XP <=10000) {
+     console.log(`${nomeDoHeroi} é classificado como: Imortal`)
+ } else if (XP >= 10001) {
+     console.log(`${nomeDoHeroi} é classificado como: Radiante`)
+ }
+
+
+   
